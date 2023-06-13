7ce618b363704913913cdee1e445d446c7485662
Addr678 <128295657+Addr678@users.noreply.github.com>
2023-06-13 11:17:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
-//complete this code
-class Rectangle {}
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-class Square extends Animal {}
+  get width() {
+    return this._width;
+  }
 
-// Do not change the code below this line
-window.Rectangle = Rectangle;
-window.Square = Square;
+  get height() {
+    return this._height;
+  }
+
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+class Square extends Rectangle {
+  constructor(sideLength) {
+    super(sideLength, sideLength);
+  }
+
+  getPerimeter() {
+    return 4 * this._width;
+  }
+}
+
+function calculateArea() {
+  const width = Number(document.getElementById('widthInput').value);
+  const height = Number(document.getElementById('heightInput').value);
+
+  const rectangle = new Rectangle(width, height);
+  const area = rectangle.getArea();
+
+  document.getElementById('areaLabel').textContent = `Area: ${area}`;
+}
+
+function calculatePerimeter() {
+  const sideLength = Number(document.getElementById('sideInput').value);
+
+  const square = new Square(sideLength);
+  const perimeter = square.getPerimeter();
+
+  document.getElementById('perimeterLabel').textContent = `Perimeter: ${perimeter}`;
+}
